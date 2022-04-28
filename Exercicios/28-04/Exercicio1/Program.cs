66140011f3ebdd970196ecb3fb3f1dff4d5eb5ef
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-04-28 20:45:14 -0300

    Exercício 1 do dia 28/04

---
+++ Exercicios/28-04/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 1) Escreva um programa em C# e no Visual Studio
+ * para calcular a média aritmética entre dois números quaisquer.
+ */
+
+Console.WriteLine("Esse programa irá receber dois números quaisquer digitados \n" +
+    "pelo usuário e calculará a média aritmética deles \n");
+
+Console.Write("Digite o primeiro número: ");
+float primeiroNumero = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+float segundoNumero = float.Parse(Console.ReadLine());
+
+Console.WriteLine("\nA média aritmética dos valores inseridos é: " +
+                    (primeiroNumero + segundoNumero) / 2);
