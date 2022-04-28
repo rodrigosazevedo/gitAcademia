df88d95c938ae4aae40a15d70238e9e9104a7de9
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-04-28 20:55:54 -0300

    Exercício 3 do dia 28/04

---
+++ Exercicios/28-04/Exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 1) Escreva um programa em C# e no Visual Studio
+ * para calcular a média aritmética entre dois números quaisquer.
+ */
+
+Console.WriteLine("Esse programa irá receber dois números quaisquer e atribuí-los \n" +
+    "às variáveis A e B. Depois, irá invertê-los (valor de A para B e do B para A) \n");
+
+Console.Write("Digite um número para a variável A: ");
+float A = float.Parse(Console.ReadLine());
+
+Console.Write("Digite um número para a variável B: ");
+float B = float.Parse(Console.ReadLine());
+
+float C = A;
+
+A = B;
+
+B = C;
+
+Console.WriteLine("\nValores invertidos. Novo valor de A: " + A +
+                   " Novo valor de B: " + B);
