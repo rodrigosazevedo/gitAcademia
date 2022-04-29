9f1d58fc1eb9b4136ccb96e93b8409c34eb13e2b
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-04-28 21:29:29 -0300

    Exercício 5 do dia 28/04

---
+++ Exercicios/28-04/Exercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * 5) Escreva um programa em C# e no Visual Studio para calcular o consumo médio 
+ * de um automóvel (medido em Km/l), sendo que são conhecidos a distância total 
+ * percorrida e o volume de combustível consumido para percorrê-la (medido em l).
+ */
+
+Console.WriteLine("Esse programa irá receber a distância percorrida e o volume \n" +
+                "de combustível consumido (em litros), e com base nisso calcular" +
+                "o consumo médio desse automóvel.\n");
+
+Console.Write("Digite a distância total percorrida: ");
+float distancia = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o volume de combustível consumido, em litros: ");
+float combustivel = float.Parse(Console.ReadLine());
+
+Console.WriteLine("O consumo médio desse automóvel é de: " +
+                  distancia / combustivel + "km/l");
