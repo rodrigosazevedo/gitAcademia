bce02b0b7815764efb372e7f1bf02a24b8889874
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-04 21:31:41 -0300

    Exercício 4 do dia 04/05

---
+++ Exercicios/04-05/Exercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 4 - Escrever um algoritmo para ler a quantidade de horas aula dadas por dois professores
+ e o valor por hora recebido por cada um deles. Mostrar na tela qual dos professores tem o 
+ maior salário total. */
+
+int horasPrimeiro, horasSegundo;
+double valorPrimeiro, valorSegundo;
+
+Console.WriteLine("Esse programa receberá a quantidade de horas e o valor/hora de dois professores. " +
+                   "\nNo final, mostrará qual deles tem o maior salário total");
+
+Console.Write("Digite a quantidade de horas trabalhada do primeiro professor: ");
+horasPrimeiro = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor/hora do primeiro professor: ");
+valorPrimeiro = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a quantidade de horas trabalhada do segundo professor: ");
+horasSegundo = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor/hora do segundo professor: ");
+valorSegundo = double.Parse(Console.ReadLine());
+
+if((horasPrimeiro * valorPrimeiro) == (horasSegundo * valorSegundo))
+{
+    Console.WriteLine("\nOs dois professores recebem o mesmo!");
+}
+else if ((horasPrimeiro * valorPrimeiro) > (horasSegundo * valorSegundo))
+{
+    Console.WriteLine("\nO primeiro professor recebe mais!");
+}
+else
+{
+    Console.WriteLine("\nO segundo professor recebe mais!");
+}
