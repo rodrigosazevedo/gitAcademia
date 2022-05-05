f4482bf41e260ab029718cbfae0d5d17edbf1df7
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-04 21:39:53 -0300

    Exercício 5 do dia 04/05

---
+++ Exercicios/04-05/Exercicio 5/Program.cs
@@ -0,0 +1,26 @@
+﻿/*5 - Escrever um algoritmo para ler duas notas de um aluno e escrever na tela a palavra
+“Aprovado” se a média das duas notas for maior ou igual a 7,0. Caso a média seja inferior a 7,0,
+o programa deve ler a nota do exame e calcular a média final. Se esta média for maior ou igual a 5,0,
+o programa deve escrever “Aprovado”, caso contrário deve escrever “Reprovado”.
+*/
+
+double notaP1, notaP2, notaExame;
+
+Console.WriteLine("Esse programa receberá duas notas de um aluno e calculará a média," +
+                  "\nmostrando se ele foi aprovado ou não depois de considerar a nota do exame.");
+
+Console.Write("Digite a primeira nota do aluno: ");
+notaP1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a segunda nota do aluno: ");
+notaP2 = double.Parse(Console.ReadLine());
+
+if ((notaP1 + notaP2) / 2 >= 7.0) Console.WriteLine("\nAprovado!");
+else
+{
+    Console.Write("\nAluno em recuperação. Digite a nota do exame: ");
+    notaExame = double.Parse(Console.ReadLine());
+
+    if (notaExame >= 5.0) Console.WriteLine("\nAprovado!");
+    else Console.WriteLine("\nReprovado!");
+}
