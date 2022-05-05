5e377a8dedda5d1e7639a9033f8415eb41e13a26
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-04 21:25:25 -0300

    Exercício 3 do dia 04/05

---
+++ Exercicios/04-05/Exercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 3 - Escrever um algoritmo para ler quatro valores inteiros, calcular a sua média,
+ e escrever na tela os que são superiores à média. */
+
+int primeiroNumero, segundoNumero, terceiroNumero, quartoNumero, mediaAritmetica;
+
+Console.WriteLine("Esse programa irá receber quatro números inteiros, calcular a média e mostrar" +
+                  "quais deles são superiores à média");
+
+Console.Write("Digite o primeiro número: ");
+primeiroNumero = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+segundoNumero = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+terceiroNumero = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o quarto número: ");
+quartoNumero = int.Parse(Console.ReadLine());
+
+mediaAritmetica = (primeiroNumero + segundoNumero + terceiroNumero + quartoNumero) / 4;
+
+Console.WriteLine("Números acima da média: ");
+
+if (primeiroNumero > mediaAritmetica) Console.WriteLine(primeiroNumero);
+if (segundoNumero > mediaAritmetica) Console.WriteLine(segundoNumero);
+if (terceiroNumero > mediaAritmetica) Console.WriteLine(terceiroNumero);
+if (quartoNumero > mediaAritmetica) Console.WriteLine(quartoNumero);
