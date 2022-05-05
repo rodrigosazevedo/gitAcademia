ead721b74c8a2a9289367a34aec97e5ce02b20bb
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-04 21:12:53 -0300

    Exercício 1 do dia 04/05

---
+++ Exercicios/03-05/ExercicioBonus1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Fazer um programa que receba o nome completo de uma pessoa e exiba em maiúsculo
+ * o primeiro nome e o último sobrenome dessa pessoa */
+
+Console.WriteLine("Esse programa irá receber o nome completo do usuário e exibir\n " +
+                    "o primeiro nome e o último sobrenome em maiúsculo");
+
+string nomeCompleto;
+string[] nomeSplit;
+
+Console.Write("Insira o nome completo da pessoa: ");
+nomeCompleto = Console.ReadLine();
+
+nomeSplit = nomeCompleto.Split();
+
+Console.WriteLine("Primeiro nome: " + nomeSplit[0] + " Último sobrenome: " + nomeSplit[nomeSplit.Length-1]);
---
+++ Exercicios/04-05/Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*1 - Faça um algoritmo para calcular e mostrar a área de um triângulo a partir dos valores da base
+ * e da altura lidos pelo teclado: Área do triangulo = (base * altura) / 2; 
+Teste se a base ou a altura digitada não foi igual a zero. */
+
+double baseTriangulo, alturaTriangulo;
+
+Console.WriteLine("Esse programa receberá o valor da altura e da base de um triângulo, e retornará a área.");
+
+Console.Write("Digite o valor da base do triângulo: ");
+baseTriangulo = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor da altura do triângulo: ");
+alturaTriangulo = double.Parse(Console.ReadLine()); 
+
+if (baseTriangulo == 0 || alturaTriangulo == 0)
+{
+    Console.WriteLine("\nAltura ou base do triângulo não podem iguais a 0");
+}
+else
+{
+    Console.WriteLine("\nÁrea do triângulo = " + (baseTriangulo * alturaTriangulo) / 2);
+}
