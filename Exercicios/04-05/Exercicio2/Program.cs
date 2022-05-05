031c6e877b46b0df19b27c14fb1bbc224f0c18cc
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-04 21:17:23 -0300

    Exercício 2 do dia 04/05

---
+++ Exercicios/04-05/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 2 - Faça um algoritmo para ler um número e verificar se ele é par ou ímpar. */
+
+Console.WriteLine("Esse programa receberá um número e dirá se ele é par ou ímpar");
+
+int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = int.Parse(Console.ReadLine());
+
+if (numeroDigitado % 2 == 0)
+{
+    Console.WriteLine("O número é par!");
+}
+else
+{
+    Console.WriteLine("O número é ímpar!");
+}
