08f10fc6e80a5c2011cfb880aed9f52c6661e290
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-12 20:53:33 -0300

    Exercício 6 do dia 12/05

---
+++ Exercicios/12-05/Exercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 6.Faça um programa em VS que solicite ao o usuário que informe a idade e valide a entrada de dados,
+ou seja, repita a leitura até que ela esteja correta (maior do que zero). 
+Ao final, mostre a idade digitada. */
+
+Console.WriteLine("Esse programa receberá a idade do usuário e a exibirá em seguida.");
+
+int idadeInformada;
+
+Console.Write("Digite a idade: ");
+idadeInformada = int.Parse(Console.ReadLine());
+
+while (idadeInformada <= 0)
+{
+    Console.WriteLine("Idade inválida! Deve ser maior que 0");
+    Console.Write("Digite a idade: ");
+    idadeInformada = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Idade digitada: " + idadeInformada + " anos.");
