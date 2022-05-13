4912386f63e8d3176447c913d6038f1c08068ab5
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-12 21:14:42 -0300

    Exercício 8 do dia 12/05

---
+++ Exercicios/12-05/Exercicio8/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Faça um programa em VS que solicite um numero inteiro. Se o numero não for inteiro, solicite-o até q
+ue seja. após, informe se ele é par ou impar. 
+Finalmente, pergunte ao usuário se ele quer repetir o programa em VS. */
+
+double numeroInformado;
+string escolhaUsuario;
+bool flagExecutar = true;
+
+do
+{
+    Console.Write("Digite um número inteiro: ");
+    numeroInformado = double.Parse(Console.ReadLine());
+
+    while (numeroInformado % 1 != 0 || numeroInformado == 0)
+    {
+        Console.Write("Número inválido (0 ou não inteiro), digite novamente: ");
+        numeroInformado = double.Parse(Console.ReadLine());
+    }
+
+    if (numeroInformado % 2 == 0) Console.WriteLine("O número é par.");
+    else Console.WriteLine("O número é ímpar.");
+
+    Console.Write("\nDeseja iniciar o programa novamente (s/n)? ");
+    escolhaUsuario = Console.ReadLine().ToUpper();
+
+    while (escolhaUsuario != "S" && escolhaUsuario != "N")
+    {
+        Console.Write("Escolha inválida. Deseja iniciar o programa novamente (s/n)? ");
+        escolhaUsuario = Console.ReadLine().ToUpper();
+    }
+
+    if (escolhaUsuario == "S") flagExecutar = true;
+    else if (escolhaUsuario == "N") flagExecutar = false;
+}
+while (flagExecutar == true);
