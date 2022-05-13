00f3ae39a1f36f0d2892bd2b9654b68705d4221d
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-13 20:26:01 -0300

    Exercício 18 do dia 12/05

---
+++ Exercicios/12-05/Exercicio18/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 18.Elaborar um programa que apresente o valor de uma potência de uma base qualquer 
+elevada a um expoente qualquer, ou seja, NM. */
+
+int baseInformada, expoenteInformado;
+
+Console.Write("Digite um número para a base: ");
+baseInformada = int.Parse(Console.ReadLine());
+
+Console.Write("Digite um número para o expoente: ");
+expoenteInformado = int.Parse(Console.ReadLine());
+
+Console.WriteLine(baseInformada + " elevado a " + expoenteInformado + " = " + Math.Pow(baseInformada, expoenteInformado));
