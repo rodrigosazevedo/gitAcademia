3f268becf1e09f5b61f85104ddee0f3c41a7cf46
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-13 20:08:42 -0300

    Exercício 15 do dia 12/05

---
+++ Exercicios/12-05/Exercicio15/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 15.Ler 4 valores referentes a notas escolares de um aluno e imprimir uma mensagem dizendo se ele está aprovado 
+ou reprovado, após ter calculado sua média ponderando, considerando pesos 2, 1, 2, e 4, respectivamente. 
+Considerar nota 7,0 como mínima para aprovação. */
+
+double primeiraNota, segundaNota, terceiraNota, quartaNota;
+
+Console.Write("Digite a primeira nota do aluno: ");
+primeiraNota = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a segunda nota do aluno: ");
+segundaNota = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a terceira nota do aluno: ");
+terceiraNota = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a quarta nota do aluno: ");
+quartaNota = double.Parse(Console.ReadLine());
+
+if ((primeiraNota * 2 + segundaNota * 1 + terceiraNota * 2 + quartaNota * 4) / 9 >= 7.0) Console.WriteLine("Aluno aprovado!");
+else Console.WriteLine("Aluno reprovado!");
