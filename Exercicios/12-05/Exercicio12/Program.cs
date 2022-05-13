74f85cff94ffc98be42a41872a1755fe584225ef
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-13 19:56:40 -0300

    Exercício 12 do dia 12/05

---
+++ Exercicios/12-05/Exercicio12/Program.cs
@@ -0,0 +1,8 @@
+﻿/* 12. Apresentar o total da soma dos cem primeiros números inteiros. */
+
+int i;
+int soma = 0;
+
+for (i = 1; i <= 100; i++) soma = soma + i;
+
+Console.WriteLine("Total da soma dos cem primeiros números inteiros: " + soma);
