4b7eadcf41561cdb4ba297eefc344c880e60efc0
rodrigosazevedo <rodrigo.sazeved@gmail.com>
2022-05-13 19:58:36 -0300

    Exercício 13 do dia 12/05

---
+++ Exercicios/12-05/Exercicio13/Program.cs
@@ -0,0 +1,9 @@
+﻿/* 13. Elaborar um programa que apresente o somatório dos valores pares existentes 
+entre 1 e 500. */
+
+int i;
+int soma = 0;
+
+for (i = 2; i <= 500; i += 2) soma = soma + i;
+
+Console.WriteLine("Total da soma dos valores pares existentes entre 1 e 500: " + soma);
